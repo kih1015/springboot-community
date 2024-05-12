213c68baf8e5a2b71bf379be697bfd18e5c82f3c
In-Hwa Kang <rkddlsghk1998@gmail.com>
2024-05-12 18:29:16 +0900

    feat: Add HelloController

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello World!";
+    }
+
+    @GetMapping("/hello2")
+    public String hello2() {
+        return "hello";
+    }
+}
