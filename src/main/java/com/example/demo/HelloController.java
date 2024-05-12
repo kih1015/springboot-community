addaae8ba32fde358811f18df246af3b1215c80a
In-Hwa Kang <rkddlsghk1998@gmail.com>
2024-05-12 22:52:22 +0900

    feat: HelloController 구현

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,20 +1,30 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class HelloController {
 
-    @ResponseBody
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello World!";
-    }
-
-    @GetMapping("/hello2")
-    public String hello2() {
+    @GetMapping("/introduce")
+    public String hello(@RequestParam(name = "name", required = false, defaultValue = "강인화") String name, Model model) {
+        model.addAttribute("name", name);
         return "hello";
     }
+
+    @ResponseBody
+    @GetMapping("/json")
+    public Map<String, Object> json() {
+        Map<String, Object> user = new HashMap<>();
+        user.put("name", "최준호");
+        user.put("age", 25);
+        return user;
+    }
+
 }
