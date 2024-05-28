ebca224ec1e4c25a16d779bd1ebac189e97c951a
In-Hwa Kang <rkddlsghk1998@gmail.com>
2024-05-28 16:30:01 +0900

    refactor: 날짜 포맷 변경

--- src/main/java/com/kang/community/controller/dto/ArticleResponse.java
+++ src/main/java/com/kang/community/controller/dto/ArticleResponse.java
@@ -1,5 +1,6 @@
 package com.kang.community.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.kang.community.domain.Article;
 
 import java.time.LocalDateTime;
@@ -10,7 +11,9 @@ public record ArticleResponse(
         Long board_id,
         String title,
         String content,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         LocalDateTime created_date,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
         LocalDateTime modified_date) {
 
     public static ArticleResponse from(Article article) {
