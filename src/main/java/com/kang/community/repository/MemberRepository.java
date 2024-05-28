15786a28e35aa6ccc281372bc90d86735f5849be
In-Hwa Kang <rkddlsghk1998@gmail.com>
2024-05-28 15:33:17 +0900

    refactor: 불필요 클래스 삭제

--- src/main/java/com/kang/community/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kang.community.repository;
-
-import com.kang.community.domain.Member;
-
-import java.util.List;
-
-public interface MemberRepository {
-
-}
--- src/main/java/com/kang/community/service/MemberService.java
+++
@@ -1,7 +0,0 @@
-package com.kang.community.service;
-
-import com.kang.community.controller.dto.MemberRequest;
-
-public interface MemberService {
-
-}
